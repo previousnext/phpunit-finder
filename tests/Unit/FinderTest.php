138bb0c98f0d35bc51a0cfa4a7f409014f4f3c4e
Kim Pepper <kim@pepper.id.au>
2019-11-19 12:04:57 +1100

    Adds support for phpunit 7 (#1)
    * Adds support for phpunit 7
    * Bump highest to php 7.3
    * Update version

---
+++ tests/Unit/FinderTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace PhpUnitFinder\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use PhpUnitFinder\FinderCommand;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * A stub test.
+ */
+class FinderTest extends TestCase {
+
+  /**
+   * A stub test.
+   */
+  public function testDiscovery() {
+    $command = new CommandTester(new FinderCommand());
+    $command->execute([]);
+    $this->assertNotNull($command->getDisplay());
+  }
+
+}
