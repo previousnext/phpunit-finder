049675eff7d534cdbd52117a68d856849e98f149
Lee Rowlands <lee.rowlands@previousnext.com.au>
2021-06-21 11:02:38 +1000

    Working version on PHPUnit 9

--- src/FinderCommand.php
+++ src/FinderCommand.php
@@ -7,6 +7,7 @@ use PHPUnit\Framework\TestSuite;
 use PHPUnit\TextUI\TestDirectoryNotFoundException;
 use PHPUnit\TextUI\TestSuiteMapper;
 use PHPUnit\TextUI\XmlConfiguration\Configuration;
+use PHPUnit\TextUI\XmlConfiguration\Loader;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -36,45 +37,20 @@ class FinderCommand extends Command {
     include_once $bootstrap;
     $testSuites = $input->getArgument('test-suite');
 
-    $phpunit_9 = FALSE;
-    if (!class_exists('\PHPUnit\Util\Configuration')) {
-      $config = (new \PHPUnit\TextUI\XmlConfiguration\Loader())->load($configFile);
-      $phpunit_9 = TRUE;
-    }
-    else {
-      $config = \PHPUnit\Util\Configuration::getInstance($configFile);
-    }
-    if (empty($testSuites)) {
-      $testSuites = $phpunit_9 ? $config->testSuite() : $config->getTestSuiteNames();
-    }
-    $testFilenames = [];
-    if ($phpunit_9) {
-      foreach (array_map(function (\PHPUnit\TextUI\XmlConfiguration\TestSuite $suite) {
-        return $suite->name();
-      }, iterator_to_array($testSuites)) as $name) {
-        try {
-          foreach ((new TestSuiteMapper())->map($testSuites, $name) as $test) {
-            if ($test instanceof TestCase) {
-              $testFilenames[] = ((new \ReflectionClass($test))->getFileName());
-            }
-          }
-        }
-        catch (TestDirectoryNotFoundException $e) {
-          continue;
-        }
-      }
-    }
-    else {
-      foreach ($testSuites as $suite) {
-        $suite = $config->getTestSuiteConfiguration($suite);
-        foreach (new \RecursiveIteratorIterator($suite->getIterator()) as $test) {
-          if ($test instanceof TestCase) {
-            $testFilenames[] = ((new \ReflectionClass($test))->getFileName());
-          }
-        }
-      }
+    $config = (new Loader())->load($configFile);
 
+    foreach ($config->testSuite() as $suite) {
+      if ($testSuites && !in_array($suite->name(), $testSuites, TRUE)) {
+        continue;
+      }
+      $testSuite = (new TestSuiteMapper)->map($config->testSuite(), $suite->name());
+      foreach (new \RecursiveIteratorIterator($testSuite) as $test) {
+        if ($test instanceof TestCase) {
+          $testFilenames[] = ((new \ReflectionClass($test))->getFileName());
+        }
+      }
     }
+
     $testFilenames = array_unique($testFilenames);
     foreach ($testFilenames as $testFilename) {
       $output->writeln($testFilename);
