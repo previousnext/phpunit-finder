3b3db0444a6412be0c3491765485c57d1c0770c6
Lee Rowlands <lee.rowlands@previousnext.com.au>
2021-06-21 10:45:27 +1000

    Add real tests
    (cherry picked from commit bb88dee84ba7536859300a634d4e0fd608161e95)

---
+++ tests/fixtures/tests/Unit/TestUnitTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace PhpUnitFinder\Tests\fixtures\tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Defines a test class.
+ */
+class TestUnitTest extends TestCase {
+
+  /**
+   * Tests something.
+   */
+  public function testSomething() {
+    $this->addToAssertionCount(1);
+  }
+
+}
